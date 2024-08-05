58bbd48dee12d0cc270b834f404fcc035d164bf2
pitersizovdev <pitersizov@gmail.com>
2024-08-05 18:23:04 +0300

    upd: logports

--- log_ports.sh
+++ log_ports.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Логирование информации с цветовым выделением
+# Получаем IP-адрес контейнера
+CONTAINER_IP=$(hostname -i)
+
+# Выводим информацию о хосте и портах
 echo -e "\033[32m============================================\033[0m"
-echo -e "\033[32mApp is running on http://localhost:${PORT}\033[0m"
-echo -e "\033[32mDatabase is running on postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}\033[0m"
+echo -e "\033[32mApp container running on $CONTAINER_IP:3000\033[0m"
+echo -e "\033[32mDB container running on: $CONTAINER_IP:5432\033[0m"
 echo -e "\033[32m============================================\033[0m"
+
+# Запускаем основное приложение
+exec "$@"
