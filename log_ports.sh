5d709546137f1a1a303ce6c86788c3589dae1971
pitersizovdev <pitersizov@gmail.com>
2024-08-06 11:42:28 +0300

    upd: guest_port_docker_bd

--- log_ports.sh
+++ log_ports.sh
@@ -6,7 +6,7 @@ CONTAINER_IP=$(hostname -i)
 # Выводим информацию о хосте и портах
 echo -e "\033[32m============================================\033[0m"
 echo -e "\033[32mApp container running on $CONTAINER_IP:3000\033[0m"
-echo -e "\033[32mDB container running on: $CONTAINER_IP:5432\033[0m"
+echo -e "\033[32mDB container running on: $CONTAINER_IP:4000\033[0m"
 echo -e "\033[32m============================================\033[0m"
 
 # Запускаем основное приложение
